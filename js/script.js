0dfd2174a4431ed938e2b6da411162cf8e392725
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-08-23 13:49:05 +0300

    add выбрать город

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
-const citySelect = document.querySelector('#city')
-const cityChoices = new Choices(citySelect, {
-    searchEnabled: false,
-    itemSelectText: '',
-});
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations"; // получить города
 
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const responce = await fetch(url);
+    const data = await responce.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError(err);
+  }
+};
+
+const init = () => {
+  const citySelect = document.querySelector("#city");
+  const cityChoices = new Choices(citySelect, {
+    // searchEnabled: false, // если удалить это, появится поиск по городам
+    itemSelectText: "",
+  });
+
+  getData(
+    `${API_URL}${LOCATION_URL}`,
+    (locationData) => {
+      const locations = locationData.map((location) => {
+        value.location;
+      });
+      cityChoices.setChoices(locations, "value", "label", false);
+    },
+    (err) => {
+      // console.log(err);
+    },
+  );
+};
+
+init();
